9129aea940064a541261e0baf33743c4af7db842
João Manoel Sobral da Costa <joaomsobral.drovak@gmail.com>
2025-09-13 19:12:32 -0300

    Added the player to the screen

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 import constants
+import player
 
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+    player_variable = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player_variable.draw(screen)
         pygame.display.flip()
+        delta_time = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
